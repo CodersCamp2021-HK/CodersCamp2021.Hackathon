47f1b06e4ccff8c294c1d622f077241241fcee34
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-06-04 21:56:02 +0200

    feat(extension): display notification on SSE

--- packages/extension/src/background/main.ts
+++ packages/extension/src/background/main.ts
@@ -1,9 +1,20 @@
+import { FactcheckApi, FactcheckEventDto } from '@faktyczka/sdk';
+
+import logo from '../../public/icons/Icon128.png';
 import { apiConfiguration } from '../shared';
 
+const factcheckApi = new FactcheckApi(apiConfiguration);
 const eventSource = new EventSource(`${apiConfiguration.basePath}/api/factchecks/sync`);
 
-eventSource.addEventListener('message', (event) => {
-  console.log(event.data);
+eventSource.addEventListener('factcheck', async (event) => {
+  const { id } = JSON.parse(event.data) as FactcheckEventDto;
+  const factcheck = await factcheckApi.findById({ id });
+  chrome.notifications.create(id, {
+    type: 'basic',
+    title: 'Czytany przez Ciebie artykuł został zweryfikowany!',
+    iconUrl: logo,
+    message: factcheck.verificationSrc,
+  });
 });
 
 export {};
