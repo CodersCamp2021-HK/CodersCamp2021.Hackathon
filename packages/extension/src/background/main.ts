3bff0c0fd92a6fe4f9edbd9277cfb12fb5c3e06e
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-06-04 22:23:24 +0200

    feat(extension): add news to storage

--- packages/extension/src/background/main.ts
+++ packages/extension/src/background/main.ts
@@ -1,4 +1,4 @@
-import { FactcheckApi, FactcheckEventDto } from '@faktyczka/sdk';
+import { FactcheckApi, FactcheckDataDto } from '@faktyczka/sdk';
 
 import logo from '../../public/icons/Icon128.png';
 import { apiConfiguration } from '../shared';
@@ -7,14 +7,22 @@ const factcheckApi = new FactcheckApi(apiConfiguration);
 const eventSource = new EventSource(`${apiConfiguration.basePath}/api/factchecks/sync`);
 
 eventSource.addEventListener('factcheck', async (event) => {
-  const { id } = JSON.parse(event.data) as FactcheckEventDto;
-  const factcheck = await factcheckApi.findById({ id });
-  chrome.notifications.create(id, {
-    type: 'basic',
-    title: 'Czytany przez Ciebie artykuł został zweryfikowany!',
-    iconUrl: logo,
-    message: factcheck.verificationSrc,
-  });
-});
+  const { id, url } = JSON.parse(event.data) as FactcheckDataDto;
 
-export {};
+  const visitsItems = await chrome.history.getVisits({ url });
+  const userSawArticle = visitsItems.length > 0;
+
+  const { factchecks } = await chrome.storage.sync.get(['factchecks']);
+  chrome.storage.sync.set({ factchecks: [...(factchecks ?? []), { id, url, inHistory: userSawArticle }] });
+
+  const { verificationSrc } = await factcheckApi.findById({ id });
+
+  if (userSawArticle) {
+    chrome.notifications.create(id, {
+      type: 'basic',
+      title: 'Czytany przez Ciebie artykuł został zweryfikowany!',
+      iconUrl: logo,
+      message: verificationSrc,
+    });
+  }
+});
