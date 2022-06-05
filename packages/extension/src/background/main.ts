3ec2825dfda08a07d883688736d5c73f3ddf77b6
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-06-05 02:48:41 +0200

    feat(extension): handle tokens in background script

--- packages/extension/src/background/main.ts
+++ packages/extension/src/background/main.ts
@@ -5,35 +5,6 @@ import { apiConfiguration, fetchCurrentUrl, readFactchecks, Status, storeFactche
 
 const factcheckApi = new FactcheckApi(apiConfiguration);
 
-let eventSource: EventSource | undefined;
-if (!eventSource || eventSource.readyState === EventSource.CLOSED) {
-  eventSource = new EventSource(`${apiConfiguration.basePath}/api/factchecks/sync`);
-}
-
-eventSource.addEventListener('factcheck', async (event) => {
-  const { id, url } = JSON.parse(event.data) as FactcheckDataDto;
-
-  const visitsItems = await chrome.history.getVisits({ url });
-  const userSawArticle = visitsItems.length > 0;
-
-  const factcheck = await factcheckApi.findById({ id });
-
-  if (userSawArticle) {
-    chrome.notifications.create(id, {
-      type: 'basic',
-      title: 'Czytany przez Ciebie artykuł został zweryfikowany!',
-      iconUrl: logo,
-      message: factcheck.verificationSrc,
-    });
-  }
-
-  await storeFactchecks([
-    ...(await readFactchecks()).filter((fc) => fc.id !== factcheck.id),
-    { ...factcheck, inHistory: userSawArticle },
-  ]);
-  await updateIcon();
-});
-
 const updateIcon = async () => {
   const url = await fetchCurrentUrl();
   const factchecks = await readFactchecks();
@@ -46,3 +17,41 @@ const updateIcon = async () => {
 
 chrome.tabs.onUpdated.addListener(updateIcon);
 chrome.tabs.onActivated.addListener(updateIcon);
+
+let eventSource: EventSource | undefined;
+(async () => {
+  if (!eventSource || eventSource.readyState === EventSource.CLOSED) {
+    const { token } = await chrome.storage.local.get(['token']);
+    eventSource = new EventSource(`${apiConfiguration.basePath}/api/factchecks/sync${token ? `?token=${token}` : ''}`);
+  }
+
+  eventSource.addEventListener('factcheck', async (event) => {
+    const { id, url } = JSON.parse(event.data) as FactcheckDataDto;
+
+    const factchecks = await readFactchecks();
+    if (!factchecks.some((fc) => fc.id === id)) {
+      const visitsItems = await chrome.history.getVisits({ url });
+      const userSawArticle = visitsItems.length > 0;
+
+      const factcheck = await factcheckApi.findById({ id });
+
+      if (userSawArticle) {
+        chrome.notifications.create(id, {
+          type: 'basic',
+          title: 'Czytany przez Ciebie artykuł został zweryfikowany!',
+          iconUrl: logo,
+          message: factcheck.verificationSrc,
+        });
+      }
+
+      await storeFactchecks([
+        ...factchecks.filter((fc) => fc.id !== factcheck.id),
+        { ...factcheck, inHistory: userSawArticle },
+      ]);
+
+      await chrome.storage.local.set({ token: id });
+    }
+
+    await updateIcon();
+  });
+})();
